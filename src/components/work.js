572cb024659f5eb45b0b6c2d4df7340d3de74a57
Jack Graymer <jackgraymer@gmail.com>
2023-01-27 17:13:32 +0100

    Changes Work to use React Hooks

--- src/components/work.js
+++ src/components/work.js
@@ -2,10 +2,9 @@ import React from "react";
 import './components.css'
 import { useState } from "react";
 
-class Work extends React.Component {
-    constructor(props){
-        super(props)
-        this.state={study: [
+const Work = () => {
+    const[state, setState] = useState({
+        work: [
             {
                 role: '',
                 company:'',
@@ -15,68 +14,62 @@ class Work extends React.Component {
                 duty:'',
                 id: crypto.randomUUID()
             }, 
-        ]}
-    }
+        ]
+    })
 
-    addStudies = () => {
-        this.setState(prevState => ({
-            study: [...prevState.study, {...this.state.study[0],role: '',company:'', place: '', from: '', to: '' , duty:'', id: crypto.randomUUID()}, ]
+    const addStudies = () => {
+        setState(prevState => ({
+            work: [...prevState.work, {...state.work[0],role: '',company:'', place: '', from: '', to: '' , duty:'', id: crypto.randomUUID()}, ]
           }))
     }
 
-    deleteStudies = (e) => {
-        if(this.state.study.length >1){
-            let newState = this.state.study.filter(a => a.id !== e.target.parentNode.id)
-            this.setState({
-                study: newState
+    const deleteStudies = (e) => {
+        if(state.work.length >1){
+            let newState = state.work.filter(a => a.id !== e.target.parentNode.id)
+            setState({
+                work: newState
             })
         }
     }
 
-    updateStudies = (e, id) => {
-        this.setState({
-            ...this.state,
-            study: this.state.study.map((input) => 
+    const updateStudies = (e, id) => {
+        setState({
+            ...state,
+            work: state.work.map((input) => 
                 e.target.parentNode.id === input.id? {...input, [e.target.classList[0]] : e.target.value}: input
             )
         })
     }
 
-    studies = () => {
+    const studies = () => {
             return(
-                this.state.study.map((obj,i)=> (
+                state.work.map((obj,i)=> (
                     <div id={obj.id} key={i}>
                         {Object.entries(obj).map((obj,i) => 
                                                
                             {if(i<6){
                                return (
-                                    <input type='text' key={i} placeholder={obj[0][0].toUpperCase() + obj[0].slice(1)} className={obj[0] + ' study'} value={obj[1]} onChange={this.updateStudies}></input>
+                                    <input type='text' key={i} placeholder={obj[0][0].toUpperCase() + obj[0].slice(1)} className={obj[0] + ' work'} value={obj[1]} onChange={updateStudies}></input>
                                 )
                                 }
                             }    
                         )}
-                       <button className="del" onClick={this.deleteStudies}>Delete</button>
+                       <button className="del" onClick={deleteStudies}>Delete</button>
                     </div>
                 ))             
             )            
     }
-
-    render = () =>{
-        return(
-            <div className="experience container">
-                <h5>Experience</h5>
-                
-                {this.studies()}
-                
-                <button onClick={this.addStudies} className='add'> Add Experience </button>
-            </div>
+    
+    return(
+        <div className="experience container">
+            <h5>Experience</h5>
             
-        )
-    }
+            {studies()}
+            
+            <button onClick={addStudies} className='add'> Add Experience </button>
+        </div>
+        
+    )
 }
-
-
-
-
 
 export default Work
